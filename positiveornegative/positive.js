a1813a79d836386f4a237ca52d2431e32cef6ec0
Abdul Rahman Khan <abdulrkhan9899@gmail.com>
2024-12-20 10:53:02 +0530

    positiveornegative

---
+++ positiveornegative/positive.js
@@ -0,0 +1,10 @@
+// Q2 Create a program that checks if a given number is positive, negative, or zero.
+function checkNumber(num) {
+  if (num > 0) {
+    return "Positive";
+  } else if (num < 0) {
+    return "Negative";
+  } else {
+    return "Zero";
+  }
+}
