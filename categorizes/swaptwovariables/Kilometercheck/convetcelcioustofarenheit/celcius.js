a153b77efcd9eb9285be739e4294a47cfd9b5304
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-03-04 16:49:14 +0530

    convert celsius to fahrenheit ques solved

---
+++ categorizes/swaptwovariables/Kilometercheck/convetcelcioustofarenheit/celcius.js
@@ -0,0 +1,9 @@
+// program to convert celsius to fahrenheit
+
+const celsius = 25;  
+
+
+const fahrenheit = (celsius * 1.8) + 32;
+
+
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
