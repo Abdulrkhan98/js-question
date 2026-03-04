d59676e33cc505cd7a213a48f7ed546b3295e63b
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-03-04 16:55:04 +0530

    sumnumbers ques solved

---
+++ categorizes/swaptwovariables/Kilometercheck/convetcelcioustofarenheit/sumnumbers/sum.js
@@ -0,0 +1,12 @@
+// program to display the sum of natural numbers
+
+const number = 5;   
+
+let sum = 0;
+
+
+for (let i = 1; i <= number; i++) {
+    sum += i;
+}
+
+console.log('The sum of natural numbers is:', sum);
