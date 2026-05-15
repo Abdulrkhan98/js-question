2ebe3a1789da1f68d46c72543f26bad272f02375
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-05-15 15:26:45 +0530

    swaptwonumbers ques solved

---
+++ swaptwovariables/swap.js
@@ -0,0 +1,52 @@
+// JavaScript Program to Swap Two Variables using Function
+
+function swapVariables(a, b){
+console.log("Before Swap");
+console.log("a =",a);
+console.log("b =", b);
+
+let temp = a;
+   a = b;
+   b = temp;
+
+ console.log("After Swap:");
+   console.log("a =", a);
+  console.log("b =", b);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function swapVariables(a, b) {
+//   console.log("Before Swap:");
+//   console.log("a =", a);
+//   console.log("b =", b);
+
+  
+//   let temp = a;
+//   a = b;
+//   b = temp;
+
+//   console.log("After Swap:");
+//   console.log("a =", a);
+//   console.log("b =", b);
+// }
+
+
+// swapVariables(10, 20);
---
+++ vowel/squaretwonumber/square.js
@@ -0,0 +1,14 @@
+// JavaScript Program to Find the Square Root
+
+
+
+
+
+function findSquareRoot(num) {
+  return Math.sqrt(num);
+}
+
+const result = findSquareRoot(25);
+
+
+document.write("Square Root is: " + result);
