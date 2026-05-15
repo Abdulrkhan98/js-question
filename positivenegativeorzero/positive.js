057ba291bcc37e48a4c5b669f22cbad8ad8df4e7
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-05-15 18:56:54 +0530

    positive negative or zero finder ques solved

--- positivenegativeorzero/positive.js
+++ positivenegativeorzero/positive.js
@@ -1,3 +1,15 @@
 //  ⁠JavaScript Program to Check if a number is Positive, Negative, or Zero
 
-let num = prompt(enter a number)
+function checkNumber(num) {
+  if (num > 0) {
+    console.log("positive number");
+  } else if (num < 0) {
+    console.log("negative number");
+  } else if (num === 0) {
+    console.log("zero");
+  }
+}
+
+checkNumber(5);  
+checkNumber(-2);  
+checkNumber(0);   
