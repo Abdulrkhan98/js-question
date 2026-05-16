6f38453b6f441f0bb585d76bb4560c1839560ef8
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-05-16 17:29:29 +0530

    fibonacci sequences ques solved

---
+++ factorial/multiplicationtable/fibonaccisequences/fibonacci.js
@@ -0,0 +1,16 @@
+// JavaScript Program to Print the Fibonacci Sequence. 
+
+function fibonacci(n) {
+  let a = 0;
+  let b = 1;
+
+  for (let i = 1; i <= n; i++) {
+    console.log(a);
+
+    let next = a + b;
+    a = b;
+    b = next;
+  }
+}
+
+fibonacci(10);
