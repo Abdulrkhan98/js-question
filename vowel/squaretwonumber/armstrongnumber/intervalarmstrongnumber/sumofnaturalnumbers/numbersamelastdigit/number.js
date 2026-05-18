18584c3cd22b44e6ea8f1e275315d65c8ee77515
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-05-18 18:42:13 +0530

    number have same last   digit ques solved

---
+++ vowel/squaretwonumber/armstrongnumber/intervalarmstrongnumber/sumofnaturalnumbers/numbersamelastdigit/number.js
@@ -0,0 +1,13 @@
+// JavaScript Program to Check if the Numbers Have Same Last Digit
+
+function checkLastDigit(a, b, c) {
+
+  if (a % 10 === b % 10 && b % 10 === c % 10) {
+    console.log("Same Last Digit");
+  } else {
+    console.log("Different Last Digit");
+  }
+
+}
+
+checkLastDigit(12, 32, 52);
