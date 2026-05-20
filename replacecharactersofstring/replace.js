de5cafc51f850ca4c28fc22409816672d6b2fee7
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-05-20 15:16:45 +0530

    replace characters of a string

---
+++ replacecharactersofstring/replace.js
@@ -0,0 +1,9 @@
+// JavaScript Program to Replace Characters of a String.
+
+// function replaceChar = (str, oldchar, newchar);
+// let str="hello world"
+// str.split(" ")
+// console.log(str.split(" ").reverse().join())
+
+let str="hello world"
+console.log(str.split(" ").reverse("").join())
