8cdb873dad5e68fb64b173e151e2096abdb71d7a
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-05-22 17:36:25 +0530

    Count the Number of Vowels in a String ques solved

---
+++ vowelsinastring/stringvowel.js
@@ -0,0 +1,26 @@
+// JavaScript Program to Count the Number of Vowels in a String
+
+function countVowels(str) {
+
+    let count = 0;
+
+    str = str.toLowerCase();
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (
+            str[i] === "a" ||
+            str[i] === "e" ||
+            str[i] === "i" ||
+            str[i] === "o" ||
+            str[i] === "u"
+        ) {
+            count++;
+        }
+
+    }
+
+    return count;
+}
+
+console.log(countVowels("Hello"));
