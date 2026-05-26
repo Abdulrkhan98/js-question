2448f0fe0d2b112e705154e30065c7bf49d3fb5e
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-05-26 16:09:14 +0530

    JavaScript Program to Create Multiline Strings ques solved

---
+++ JavaScript Program to Create Multiline Strings/program.js
@@ -0,0 +1,12 @@
+// JavaScript Program to Create Multiline Strings.
+
+function multilineString() {
+
+    let str = `Hello
+Welcome to JavaScript
+Frontend Development`;
+
+    return str;
+}
+
+console.log(multilineString());
