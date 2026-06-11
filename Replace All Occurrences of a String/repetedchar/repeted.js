80d18fef07df3dbc127cc2056b0e3aed13cec56d
Abdul Rahman Khan <Abdulrkhan9899@gmail.com>
2026-06-11 18:12:04 +0530

    program to count the character frequency count

---
+++ Replace All Occurrences of a String/repetedchar/repeted.js
@@ -0,0 +1,14 @@
+// write a program to count the character frequency count 
+function repetedChar(arr){
+    let obj = {}
+    for(let i=0; i< arr.length; i++){
+     if(obj[arr[i]]){
+         obj[arr[i]]+=1
+     }else{
+         obj[arr[i]]=1
+     }
+    }
+    return obj
+   
+}
+console.log(repetedChar("ggggffffbgg"))
